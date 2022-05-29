93cf44ba110ef7a053ed0aa8cb1c14f44133db4b
Akos Hochrein <hoch.akos@gmail.com>
2022-05-30 00:24:05 +0200

    better footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,17 +1,39 @@
-import { Container } from "@mui/material";
-import { styled } from "@mui/material/styles";
-
-const FooterWrapper = styled((props: { children: (string | number)[] }) => (
-    <Container component="footer" {...props} />
-))`
-    display: flex;
-    justify-content: space-around;
-`;
+import { Container, IconButton, Stack, Typography } from "@mui/material";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export const Footer = () => {
     return (
-        <FooterWrapper>
-            © Akos Hochrein, {new Date().getFullYear()}
-        </FooterWrapper>
+        <Container component="footer">
+            <Stack direction="column" textAlign="center" gap={1}>
+                <Stack direction="row" gap={2} justifyContent="center">
+                    <IconButton
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://twitter.com/akoshochrein"
+                    >
+                        <TwitterIcon />
+                    </IconButton>
+                    <IconButton
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://www.linkedin.com/in/akoshochrein/"
+                    >
+                        <LinkedInIcon />
+                    </IconButton>
+                    <IconButton
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://github.com/akoshochrein"
+                    >
+                        <GitHubIcon />
+                    </IconButton>
+                </Stack>
+                <Typography variant="body2">
+                    © Akos Hochrein, {new Date().getFullYear()}
+                </Typography>
+            </Stack>
+        </Container>
     );
 };
