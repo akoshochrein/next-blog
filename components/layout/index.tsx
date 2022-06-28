bbdb2b93d37abf95a26e95bdacd5237c44044215
Akos Hochrein <hoch.akos@gmail.com>
2022-06-28 21:46:47 +0200

    fix inline tags again

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -4,7 +4,7 @@ import { Header } from "./Header";
 
 const Layout = ({ children }) => {
     return (
-        <Box component="div">
+        <Box component="div" paddingX={2}>
             <Header />
             <Container>{children}</Container>
             <Footer />
